3124a4d0932a38278f82136bef97e7437272c148
Florent Messa <florent.messa@gmail.com>
2017-11-08 10:16:01 +0100

    Fix Django 1.11 compatibility

--- caching/compat.py
+++ caching/compat.py
@@ -5,18 +5,6 @@ import django
 from django.core.cache import cache as default_cache
 from django.core.cache.backends.base import InvalidCacheBackendError
 
-__all__ = ['DEFAULT_TIMEOUT', 'FOREVER']
-
-
-if django.VERSION[:2] >= (1, 6):
-    from django.core.cache.backends.base import DEFAULT_TIMEOUT as DJANGO_DEFAULT_TIMEOUT
-    DEFAULT_TIMEOUT = DJANGO_DEFAULT_TIMEOUT
-    FOREVER = None
-else:
-    DEFAULT_TIMEOUT = None
-    FOREVER = 0
-
-
 try:
     if django.VERSION[:2] >= (1, 7):
         from django.core.cache import caches
